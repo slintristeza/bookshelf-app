ea800ae3ae395a3891bf8139ef8287bb34ba2b12
slintristeza <slintristeza@gmail.com>
2019-08-29 19:10:17 +0900

    feat:firebase.js

---
+++ plugins/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'API_KEY',
+    authDomain: 'APP_ID.firebaseapp.com',
+    databaseURL: 'https://PROJECT_ID.firebaseio.com',
+    projectId: 'PROJECT_ID',
+    storageBucket: 'PROJECT_ID.appspot.com',
+    messagingSenderId: '1234567890'
+  })
+}
+
+export default firebase
